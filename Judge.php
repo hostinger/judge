ec17a8bf5f5f3b31b8d53d11fef3aafe4f576d9c
Skirmantas <skirmantas.juraska@hostinger.com>
2016-05-16 16:36:25 +0300

    First release version

---
+++ Judge.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ *
+ * Check if this word is allowed in Hostinger network
+ *
+ * @package Judge
+ */
+class Judge
+{
+    private $database;
+
+    public function __construct()
+    {
+        $this->database = json_decode(file_get_contents(__DIR__ . '/data.json'), 1);
+    }
+
+    /**
+     * Check if given word is allowed
+     *
+     * @param string $word
+     * @return boolean
+     * @throws Exception
+     */
+    public function isValid($word)
+    {
+        // check regexp
+        foreach ($this->database['reserved_app_names']['regexp'] as $record) {
+            if (preg_match("/$record/i", $word)) throw new Exception("This word " . $word . " is forbidden");
+        }
+        // check by phishing words
+        foreach ($this->database['reserved_app_names']['phishing_words'] as $record) {
+            if (stripos($word, $record) !== false) throw new Exception("This word " . $word . " is forbidden");
+        }
+        // check by brands
+        foreach ($this->database['reserved_app_names']['brands'] as $record) {
+            if (stripos($word, $record) !== false) throw new Exception("This word " . $word . " is forbidden");
+        }
+        return true;
+    }
+}
+
