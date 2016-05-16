e77c5fb85e39462bef097a11c3d26182c6f5cd4f
Skirmantas <skirmantas.juraska@hostinger.com>
2016-05-16 16:44:39 +0300

    Change: return false instead exception

--- Judge.php
+++ Judge.php
@@ -25,15 +25,15 @@ class Judge
     {
         // check regexp
         foreach ($this->database['reserved_app_names']['regexp'] as $record) {
-            if (preg_match("/$record/i", $word)) throw new Exception("This word " . $word . " is forbidden");
+            if (preg_match("/$record/i", $word)) return false;
         }
         // check by phishing words
         foreach ($this->database['reserved_app_names']['phishing_words'] as $record) {
-            if (stripos($word, $record) !== false) throw new Exception("This word " . $word . " is forbidden");
+            if (stripos($word, $record) !== false) return false;
         }
         // check by brands
         foreach ($this->database['reserved_app_names']['brands'] as $record) {
-            if (stripos($word, $record) !== false) throw new Exception("This word " . $word . " is forbidden");
+            if (stripos($word, $record) !== false) return false;
         }
         return true;
     }
